fdf3530e7cf9d68e7db05b3bff7d8db232997865
JiaruiXiong <56058784+JiaruiXiong@users.noreply.github.com>
2020-02-22 12:30:54 -0800

    two glider guns

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ Background.prototype.update = function () {
 
 
 
-// MushroomDude class
+// GameLife class
 function GameLife(game,width, height) {
     this.width = width;
     this.height = height;
@@ -37,53 +37,104 @@ function GameLife(game,width, height) {
     //randomize 0 or 1 in the grid
     for (let i = 0; i < this.cols; i++) {
         for (let j = 0; j < this.rows; j++) {
-            if ((i === 4 && j === 4)
-                ||  (i === 4 && j === 5)
-                // ||  (i === 0 && j === 1)
-                ||  (i === 4 && j === 6)
-                // ||  (i === 2 && j === 1 )
-            //     ||  (i === 2 && j === 6)
-            //     ||  (i === 10 && j === 5)
-            //     ||  (i === 10 && j === 6)
-            //     ||  (i === 10 && j === 7)
-            //     ||  (i === 11 && j === 8)
-            //     ||  (i === 12 && j === 9)
-            //     ||  (i === 13 && j === 9)
+            if (    // 2*2 left square
+                    (i === 40 && j === 5)
+                ||  (i === 41 && j === 5)
+                ||  (i === 40 && j === 6)
+                ||  (i === 41 && j === 6)
+
+                    // second part of glider gun
+                ||  (i === 51 && j === 3)
+                ||  (i === 51 && j === 4)
+                ||  (i === 51 && j === 8)
+                ||  (i === 51 && j === 9)
+                ||  (i === 53 && j === 4)
+                ||  (i === 53 && j === 8)
+                ||  (i === 54 && j === 5)
+                ||  (i === 54 && j === 6)
+                ||  (i === 54 && j === 7)
+                ||  (i === 55 && j === 5)
+                ||  (i === 55 && j === 6)
+                ||  (i === 55 && j === 7)
                 
-            //     ||  (i === 11 && j === 4)
-            //     ||  (i === 12 && j === 3)
-            //     ||  (i === 13 && j === 3)
+                // third part of glider gun
+                ||  (i === 58 && j === 8)
+                ||  (i === 59 && j === 7)
+                ||  (i === 59 && j === 8)
+                ||  (i === 59 && j === 9)
+                ||  (i === 60 && j === 6)
+                ||  (i === 60 && j === 10)
+                ||  (i === 61 && j === 8)
+                ||  (i === 62 && j === 5) 
+                ||  (i === 63 && j === 5) 
+                ||  (i === 62 && j === 11)
+                ||  (i === 63 && j === 11)
+                ||  (i === 64 && j === 6)
+                ||  (i === 64 && j === 10)
+                ||  (i === 65 && j === 7)
+                ||  (i === 65 && j === 8) 
+                ||  (i === 65 && j === 9)
 
-            //     ||  (i === 14 && j === 6)
-            //     ||  (i === 15 && j === 4)
-            //     ||  (i === 16 && j === 5)
-            //     ||  (i === 16 && j === 6)
-            //     ||  (i === 17 && j === 6)
-            //     ||  (i === 16 && j === 7)
-            //     ||  (i === 15 && j === 8)
+                // fourth part of glider gun
+                ||  (i === 74 && j === 7) 
+                ||  (i === 74 && j === 8) 
+                ||  (i === 75 && j === 7) 
+                ||  (i === 75 && j === 8) 
 
-            //     ||  (i === 20 && j === 5)
-            //     ||  (i === 20 && j === 4)
-            //     ||  (i === 20 && j === 3)
-            //     ||  (i === 21 && j === 5)
-            //     ||  (i === 21 && j === 4)
-            //     ||  (i === 21 && j === 3)
-            //     ||  (i === 22 && j === 2)
-            //     ||  (i === 24 && j === 2)
-            //     ||  (i === 24 && j === 1)
+                // second gun
+                ||  (i === 1 && j === 5)
+                ||  (i === 2 && j === 5)
+                ||  (i === 1 && j === 6)
+                ||  (i === 2 && j === 6)
 
-            //     ||  (i === 22 && j === 6)
-            //     ||  (i === 24 && j === 6)
-            //     ||  (i === 24 && j === 7)
+                    // second part of 2nd glider gun
+                ||  (i === 11 && j === 5)
+                ||  (i === 11 && j === 6)
+                ||  (i === 11 && j === 7)
+                ||  (i === 12 && j === 4)
+                ||  (i === 12 && j === 8)
+                ||  (i === 13 && j === 3)
+                ||  (i === 14 && j === 3)
+                ||  (i === 13 && j === 9)
+                ||  (i === 14 && j === 9)
+                ||  (i === 15 && j === 6)
+                ||  (i === 16 && j === 4)
+                ||  (i === 16 && j === 8)
+                ||  (i === 17 && j === 5)
+                ||  (i === 17 && j === 6)
+                ||  (i === 17 && j === 7)
+                ||  (i === 18 && j === 6)
 
-            //     ||  (i === 34 && j === 4)
-            //     ||  (i === 34 && j === 3)
-            //     ||  (i === 35 && j === 4)
-            //     ||  (i === 35 && j === 3)
+                
+                // third part of 2nd glider gun
+                ||  (i === 21 && j === 5)
+                ||  (i === 21 && j === 4)
+                ||  (i === 21 && j === 3)
+                ||  (i === 22 && j === 5)
+                ||  (i === 22 && j === 4)
+                ||  (i === 22 && j === 3)
+                ||  (i === 23 && j === 2)
+                ||  (i === 23 && j === 6) 
+                ||  (i === 25 && j === 1) 
+                ||  (i === 25 && j === 2)
+                ||  (i === 25 && j === 6)
+                ||  (i === 25 && j === 7)
+    
+
+                // fourth part of 2nd glider gun
+                ||  (i === 35 && j === 3) 
+                ||  (i === 35 && j === 4) 
+                ||  (i === 36 && j === 3) 
+                ||  (i === 36 && j === 4) 
+
+
                 
                 ) {
                     this.grid[i][j] = 1;
 
+            }
+            else {
+                this.grid[i][j] = 0;
             }
             // this.grid[i][j] =1//Math.floor(Math.random() * 2);
         }
@@ -99,8 +150,8 @@ GameLife.prototype.draw = function () {
             let x = i * this.resolution;
             let y = j * this.resolution;
             if (this.grid[i][j] === 1) {
-                this.ctx.fillStyle = generateRandomColor();
-                // this.ctx.fillStyle = 'White';
+                // this.ctx.fillStyle = generateRandomColor();
+                this.ctx.fillStyle = 'White';
                 this.ctx.fillRect(x,y,this.resolution-1, this.resolution-1);
             }
         }
@@ -113,9 +164,10 @@ GameLife.prototype.update = function () {
     for (let i = 0; i < this.cols; i++) {
         for (let j = 0; j < this.rows; j++) {
           let state = this.grid[i][j];
-
+          console.log(state);
           let neighbors = countNeighbors(this.grid, i, j,this.cols, this.rows);
           if (state === 0 && neighbors  === 3) {
+            // console.log("haha: "+i,j);
             next[i][j] = 1;
          
           }
@@ -162,17 +214,22 @@ function generateRandomColor()
 function countNeighbors(grid, x, y,cols, rows) {
     let sum = 0;
     // console.log(sum);
-    // console.log(grid[-1]);
+    // console.log(cols);
+    // console.log(grid[-1][-1]);
     for (let i = -1; i <2; i++) {
       for (let j = -1; j < 2; j++) {
         let col = (x+i+cols) % cols;
-        let row = (y+j+rows) % rows;
-        // console.log(rol);
+        let row = (y+j +rows) % rows;
+        // console.log(col,row);
         sum += grid[col][row];
+
+
+        // console.log(rol);
+
       } 
     }
     // console.log(sum);
-    // sum -= grid[x][y];
+    sum -= grid[x][y];
     return sum;
 }
 
